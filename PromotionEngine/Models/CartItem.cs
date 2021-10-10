62b95ab0142c6f272e3508e0b731604776b0594c
Oleg G <ogerasimov@btinternet.com>
2021-10-10 10:57:12 +0100

    cart: methods implemented

---
+++ PromotionEngine/Models/CartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Models
+{
+    public class CartItem
+    {
+        public SKU Sku { get; set; }
+        public uint Count { get; set; }
+        public bool PromotionApplied { get; set; } = false;
+        public decimal SubtotalAfterPromotion { get; set; }
+    }
+}
